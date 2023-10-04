a538568468ba97f7ebd648059ce26a54e449c2fc
Seng Wayha <131443243+wayha1@users.noreply.github.com>
2023-10-04 15:12:28 +0700

    update

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -7,31 +7,35 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="nav">
-      <div className="w-screen py-3 font-bold">
-        <div className="flex justify-between flex-row">
-      
-          <Link to={"/"} className="logo lg:ml-64 md:ml-12 sm:ml-12 xs:ml-12 ">
-            E-libra
-          </Link>
+      <div className="w-screen py-2 font-bold ">
+        <div className="flex justify-between lg:mx-10 md:mx-10 sm:mx-4 xs:mx-4">
+          <div className="lg:ml-32 md:ml-12 sm:ml-12 xs:ml-12 whitespace-normal flex space-x-5 ">
+            <Link to={"/"} className="logo ">
+              E-libra
+            </Link>
 
-          <div className="flex justify-between ">
-            <input
-              className="border lg:w-64 md:w-64 px-2 rounded-lg border-3 border-gray-900 "
-              type="search"
-              placeholder="Search"
-              BiSearchAlt2="absolute mt-1  "
-            />
+            <div className="flex ">
+              <input
+                className="border px-2 rounded-lg border-3 border-gray-900 flex py-0.5"
+                type="search"
+                placeholder="Search"
+                id="search"
+                
+              />
+              <BiSearchAlt2 className="absolute flex mx-48 mt-2 " />
+            </div>
           </div>
-
-          <div className="flex px-20">
+          <div className="flex ">
             <ul className="flex ">
-              <Link to={"/help"}  className="help  px-5">Help</Link>
+              <Link to={"/help"} className="help  px-5">
+                Help
+              </Link>
               <Link to={"/contact"} className="Contact px-5 ">
                 Contact
               </Link>
               <Link to={"/account"} className="Account flex px-5 ">
                 {" "}
-                <BiUserCircle className="mt-1 -translate-x-2" to={"/account"} /> 
+                <BiUserCircle className="mt-1 -translate-x-2" to={"/account"} />
                 Account
               </Link>
 
