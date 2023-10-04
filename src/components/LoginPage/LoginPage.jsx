8a96158c6895b07deb1151dbb587699b9c2e7788
Seng Wayha <131443243+wayha1@users.noreply.github.com>
2023-10-04 13:32:12 +0700

    Newversion

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -74,6 +74,74 @@ const LoginPage = () => {
         </main>
       </div>
     </form>
+    // <form className="flex flex-col item-center justify-center min-h-screen py-2 bg-gray-100">
+    //   <main className="flex-col flex item-center justify-center w-full h-full px-96 flex-1 ">
+    //     <div className="bg-white rounded-2xl flex">
+    //       <div className="">
+    //         <h1 className="text-center text-4xl pt-5">Log In</h1>
+
+    //         <div className="username mt-8 text-center w-full flex justify-center ">
+    //           <div className="flex">
+    //             <BiUserCircle className="mt-1" />
+    //             <label className="pl-2">Username</label>
+    //             <div className="pl-2">
+    //               <input
+    //                 className="username border border-indigo-500 rounded-lg pl-5 text-gray-200 "
+    //                 type="username"
+    //                 placeholder="username or email"
+    //               />
+    //             </div>
+    //           </div>
+    //         </div>
+    //         <div className="passowrd mt-8 text-center w-full flex justify-center ">
+    //           <div className="flex items-between">
+    //             <RiLockPasswordLine className="mt-1" />
+    //             <label className="pl-2">Password </label>
+    //             <div className="pl-2">
+    //               <input
+    //                 className="password border border-indigo-500 rounded-lg pl-5 text-gray-200 "
+    //                 type="password"
+    //                 placeholder="password"
+    //               />
+    //             </div>
+    //           </div>
+    //         </div>
+
+    //         <div className="w-full px-32">
+    //           <div className="mt-10 text-center justify-center bg-cyan-500 rounded-lg ">
+    //             <button type="submit" className="py-2 text-white text-2xl">
+    //               Log in
+    //             </button>
+    //           </div>
+    //         </div>
+
+    //         <div className="mt-10 text-center flex justify-center w-full rounded-lg pb-5">
+    //           <div className="flex">
+    //             <h1 className="py-3 ">Forgot Password </h1>
+    //             <button className="py-3 text-blue" type="Register">
+    //               / Register
+    //             </button>
+    //           </div>
+    //         </div>
+
+    //         <div className=" w-full flex space-x-20 px-5 py-3">
+    //           <button>
+    //             <img src={IMAGES.imgFb} alt="my image" width={50} height={50} onClick={this} />
+    //           </button>
+    //           <button>
+    //             <img src={IMAGES.imgApple} alt="my image" width={50} height={50} onClick={this} />
+    //           </button>
+    //           <button>
+    //             <img src={IMAGES.imgGoogle} alt="my image" width={50} height={50} onClick={this} />
+    //           </button>
+    //           <button>
+    //             <img src={IMAGES.imgPhone} alt="my image" width={50} height={50} onClick={this} />
+    //           </button>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </main>
+    // </form>
   );
 };
 
