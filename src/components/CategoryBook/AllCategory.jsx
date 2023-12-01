fba5c15cb324edfc7e469cd64b6c6a869d57eaa8
Seng Wayha <vehaseng1122@gmail.com>
2023-12-01 09:32:20 +0700

    update

--- src/components/CategoryBook/AllCategory.jsx
+++ src/components/CategoryBook/AllCategory.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import BacIIBook from './BacIIBook'
-import ComicBook from './ComicBook'
-import NovelBook from './NovelBook'
-import StudyBook from './StudyBook'
+import React from "react";
+import BacIIBook from "./BacIIBook";
+import ComicBook from "./ComicBook";
+import NovelBook from "./NovelBook";
+import StudyBook from "./StudyBook";
 
 const AllCategory = () => {
-
   return (
-   <div>
+    <div className="h-full overflow-y-auto">
       <h2 className="text-5xl font-bold mb-4 mt-8 ml-5 uppercase">All Categories</h2>
-      <div className='p-2'>
+      <div className="p-2">
         <div className="">
-            <BacIIBook className="" />
-          </div>
-          <div className="">
-            <ComicBook className="" />
-          </div>
-          <div>
-            <StudyBook />
-          </div>
-          <div>
-            <NovelBook />
-          </div>
+          <BacIIBook className="" />
+        </div>
+        <div className="">
+          <ComicBook className="" />
+        </div>
+        <div>
+          <StudyBook />
+        </div>
+        <div>
+          <NovelBook />
+        </div>
+        {/* Add more components or content as needed */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AllCategory
+export default AllCategory;
