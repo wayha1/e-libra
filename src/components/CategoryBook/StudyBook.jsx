c59832f0278af3817a64334e2d2f52dd09b87450
JackyDom <sopheaoudom8@gmail.com>
2023-12-08 13:23:59 +0700

    update

--- src/components/CategoryBook/StudyBook.jsx
+++ src/components/CategoryBook/StudyBook.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase";
+//js
 
 const StudyBook = () => {
   const [bacData, setBacData] = useState([]);
