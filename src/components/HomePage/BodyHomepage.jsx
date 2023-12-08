aa9b74978c73bae0e343482477ada8f78e9a054e
Seng Wayha <131443243+wayha1@users.noreply.github.com>
2023-12-08 11:33:23 +0700

    update

--- src/components/HomePage/BodyHomepage.jsx
+++ src/components/HomePage/BodyHomepage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import { db } from "../../firebase";
 import "../../App.css";
 import {
