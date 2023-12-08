53dc1d45dc4f8c707e96ddfade71edd85f108665
JackyDom <sopheaoudom8@gmail.com>
2023-12-08 13:21:53 +0700

    update

--- src/components/CategoryBook/ComicBook.jsx
+++ src/components/CategoryBook/ComicBook.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase";
+//js
 
 const ComicBook = () => {
   const [bacData, setBacData] = useState([]);
