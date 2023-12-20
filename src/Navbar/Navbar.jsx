c4bda87e14fc94e6030c80bed21852c640d59859
JackyDom <sopheaoudom8@gmail.com>
2023-12-20 15:16:56 +0700

    update

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { BiUserCircle } from "react-icons/bi";
 import { BsCartPlus } from "react-icons/bs";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
@@ -6,113 +6,106 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [nav, setNav] = useState(true);
+  const [open, setOpen] = useState(false);
+  const Menus = ["Setting", "Logout"];
+  const menuRef = useRef();
+  const imgRef = useRef();
 
   const handleNav = () => {
     setNav(!nav);
   };
 
+  const handleImageClick = (e) => {
+    e.stopPropagation();
+    setOpen(!open);
+  };
+
+  const handleMenuItemClick = () => {
+    setOpen(false);
+  };
+
   return (
     <nav className="nav-bar w-full sticky top-0 z-50 bg-gray-100 shadow-sm">
-      <div className="">
+      <div className="container mx-auto">
         {/* Desktop Mode */}
-        <div className="flex justify-evenly w-full lg:flex sm:flex md:flex">
-          <div className="flex space-x-4 items-center">
+        <div className="flex justify-between items-center py-4">
+          <div className="flex items-center space-x-4">
             <Link to={"/"}>
               <img
                 src={require("./Logo.png")}
                 alt="Logo"
                 width={40}
                 height={40}
-                className="hidden md:flex  hover:shadow-full hover:scale-110"
+                className="hidden md:flex hover:shadow-full hover:scale-110"
               />
             </Link>
-            <Link to={"/"} className="uppercase whitespace-nowrap  hover:shadow-full hover:scale-110">
+            <Link
+              to={"/"}
+              className="uppercase whitespace-nowrap hover:shadow-full hover:scale-110"
+            >
               E-libra
             </Link>
-
-            {/* Adjusted indentation here */}
-            {/* <div className="relative ...">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              </div>
-              <input
-                className="border w-52 md:w-44 sm:w-32 max-sm:w-32 px-3 mt-1 h-8 rounded-xl border-gray-900 shadow-xl shadow-inner hover:shadow-lg"
-                type="search"
-                placeholder="Search"
-                id="search"
-              />
-            </div> */}
           </div>
 
-          <div className="flex items-center">
-            <ul className="flex uppercase hidden lg:flex ">
-              <Link to={"/allgen"} className="whitespace-nowrap px-5 hover:shadow-full hover:scale-110">
-                Category Book
-              </Link>
-              <Link to={"/aboutus"} className="whitespace-nowrap px-5 hover:shadow-full hover:scale-110">
-                About Us
-              </Link>
-              <Link to={"/author"} className="px-5 hover:shadow-full hover:scale-110">
-                Author
-              </Link>
-              <Link to={"/account"} className="flex px-5 hover:shadow-full hover:scale-110">
-                {" "}
-                <BiUserCircle className="mt-1 -translate-x-2" to={"/account"} />
-                Account
-              </Link>
-              <Link to="/cart" className="flex ps-5 space-x-4 hover:shadow-full hover:scale-110">
-                <BsCartPlus className="mt-1 -translate-x-2 " to="/cart" />
-                Cart
-              </Link>
-            </ul>
-          </div>
-
-          {/* Mobile Mode */}
-          <div className="dropdown_nav z-50 w-fit h-full">
-            <div onClick={handleNav} className="block lg:hidden translate-x-5">
-              {nav ? (
-                <AiOutlineMenu size={30} className="flex m-2 " />
-              ) : (
-                <AiOutlineClose size={30} className="flex m-2" />
-              )}
-            </div>
-            <div
-              className={
-                nav
-                  ? "fixed left-[-100%] "
-                  : "fixed left-0 top-0 w-[70%] h-screen shadow-xl bg-white ease-in-out duration-500 absolute"
-              }
+          <div className="flex items-center space-x-4 mr-10">
+            <Link
+              to={"/allgen"}
+              className="whitespace-nowrap px-4 py-2 hover:shadow-full hover:scale-110"
             >
-              <Link
-                to="/"
-                className="flex items-center justify-center shadow-inner hover:shadow-full hover:scale-125"
-              >
-                <img src={require("./Logo.png")} alt="Logo" width={50} height={50} />
-                <span className="text-2xl font-semibold whitespace-nowrap text-black">E-Library</span>
-              </Link>
-              <div className="text-center ">
-                <ul className="uppercase p-4 ">
-                  <li className=" border-b border-b-slate-200 p-5 hover:shadow-full hover:scale-110">
-                    <Link to={"/aboutus"} className="font-bold whitespace-nowrap">
-                      About Us
-                    </Link>
-                  </li>
-                  <li className=" border-b border-b-slate-200 p-5  hover:shadow-full hover:scale-110">
-                    <Link to={"/author"} className="font-bold ">
-                      Author
-                    </Link>
-                  </li>
-                  <li className=" border-b border-b-slate-200 p-5 hover:shadow-full hover:scale-110">
-                    <Link to={"/account"} className="font-bold ">
-                      Account
-                    </Link>
-                  </li>
-                  <li className=" border-b border-b-slate-200 p-5  hover:shadow-full hover:scale-110">
-                    <Link to={"/cart"} className="font-bold ">
-                      Cart
-                    </Link>
-                  </li>
-                </ul>
-              </div>
+              Category Book
+            </Link>
+            <Link
+              to={"/aboutus"}
+              className="whitespace-nowrap px-4 py-2 hover:shadow-full hover:scale-110"
+            >
+              About Us
+            </Link>
+            <Link
+              to={"/author"}
+              className="px-4 py-2 hover:shadow-full hover:scale-110"
+            >
+              Author
+            </Link>
+            <Link
+              to="/cart"
+              className="px-4 py-2 hover:shadow-full flex hover:scale-110"
+            >
+              <BsCartPlus className="mt-1 -translate-x-2" />
+              Cart
+            </Link>
+            {/* Integrated Account component */}
+            <div className="relative">
+              <img
+                ref={imgRef}
+                onClick={handleImageClick}
+                src="https://th.bing.com/th/id/R.0f176a0452d52cf716b2391db3ceb7e9?rik=yQN6JCCMB7a4QQ&pid=ImgRaw&r=0"
+                alt="user"
+                className="h-14 w-18 object-cover border-4 border-gray-400 cursor-pointer rounded-full"
+              />
+              {open && (
+                <div
+                  ref={menuRef}
+                  className="bg-gray-200 p-4 w-52 shadow-lg absolute -left-14 top-20"
+                >
+                  <ul>
+                    {Menus.map((menu) => (
+                      <Link
+                        key={menu}
+                        onClick={() => {
+                          handleMenuItemClick();
+                          if (menu === "Setting") {
+                            // Navigate to the AccountPage when "Setting" is clicked
+                            window.location.href = "/account";
+                          }
+                        }}
+                        className="p-2 text-lg cursor-pointer rounded hover:bg-blue-100"
+                      >
+                        {menu}
+                      </Link>
+                    ))}
+                  </ul>
+                </div>
+              )}
             </div>
           </div>
         </div>
