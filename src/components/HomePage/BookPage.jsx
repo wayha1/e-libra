5d6e98f31676d8224f86019849704d3ed8f5dec2
Seng Wayha <vehaseng1122@gmail.com>
2023-12-22 19:12:42 +0700

    update

--- src/components/HomePage/BookPage.jsx
+++ src/components/HomePage/BookPage.jsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import { Document, Page, pdfjs } from "react-pdf";
 import "./BookPage.css"; // Import the CSS file for your styles
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
 
 // Set worker source URL
 const workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
@@ -25,40 +26,45 @@ const BookPage = () => {
     }
   };
 
-  const renderPdf = () => (
-    <div className="flex h-[980px] justify-center items-center relative">
-      <Document file={pages.BookPdf} onLoadSuccess={() => setCurrentPage(1)} className="flex">
-        {/* Display the current page and the next page */}
-        <Page pageNumber={currentPage} className="flex mr-2 w-[500px]" />
-        {currentPage + 1 <= (pages?.BookPdf?.length ?? 0) && (
-          <Page pageNumber={currentPage + 1} className="flex w-[500px]" />
-        )}
-      </Document>
+  const renderPdf = () => {
+    if (!pages?.BookPdf || pages.BookPdf.length === 0) {
+      return <p>No pages in the book</p>;
+    }
 
-      <div className="absolute bottom-0 left-0 right-0 flex justify-between mb-8">
-        <button
-          onClick={handlePreviousPage}
-          disabled={currentPage <= 1}
-          className="bg-gray-600 text-white px-4 py-2 rounded-md"
-        >
-          Prev Page
-        </button>
-        <button
-          onClick={handleNextPage}
-          disabled={currentPage >= (pages?.BookPdf?.length ?? 0)}
-          className="bg-gray-600 text-white px-4 py-2 rounded-md"
-        >
-          Next Page
-        </button>
+    const totalPages = pages.BookPdf.length;
+
+    if (currentPage < 1 || currentPage > totalPages) {
+      return <p>Invalid page number</p>;
+    }
+
+    return (
+      <div className="flex h-[980px] justify-center items-center relative">
+        <Document file={pages.BookPdf} onLoadSuccess={() => setCurrentPage(1)} className="flex">
+          {/* Display the current page and the next page */}
+          <Page pageNumber={currentPage} className="flex mr-2 w-[500px]" />
+          {currentPage + 1 <= totalPages && <Page pageNumber={currentPage + 1} className="flex w-[500px]" />}
+        </Document>
+
+        <div className="absolute bottom-0 left-0 right-0 flex justify-between mb-8">
+          <button
+            onClick={handlePreviousPage}
+            disabled={currentPage <= 1}
+            className="bg-gray-600 text-white px-4 py-2 rounded-md"
+          >
+            Prev Page
+          </button>
+          <button
+            onClick={handleNextPage}
+            disabled={currentPage + 1 > totalPages}
+            className="bg-gray-600 text-white px-4 py-2 rounded-md"
+          >
+            Next Page
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 
-  return (
-    <div className="flex h-[970px] justify-center items-center bg-gray-600">
-      {pages.BookPdf && renderPdf()}
-    </div>
-  );
+  return <div className="flex h-[970px] justify-center items-center bg-gray-600">{renderPdf()}</div>;
 };
-
 export default BookPage;
