ab50950cb4de148e5d9373f32a53d64c414d9799
Seng Wayha <vehaseng1122@gmail.com>
2023-12-22 12:01:04 +0700

    update

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { ClipLoader } from "react-spinners";
-
+import "./LoadingPage.css";
 const LoadingPage = () => {
   const [isLoading, setIsLoading] = useState(false);
 
@@ -14,23 +13,24 @@ const LoadingPage = () => {
   return (
     <div className="w-screen h-screen bg-gray-100">
       <div className="flex w-full h-full justify-center items-center">
-        <ClipLoader
-          className="flex justify-center items-center w-full h-full"
-          loading={isLoading}
-          color={545958}
-          size={150}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        ></ClipLoader>
-        <div className={`flex lg:text-5xl text-gray-600 font-semibold tracking-wide font-mono  ml-4 ${isLoading ? "animate-pulse" : ""}`}>
-        <h1 className="flex mt-10">
-          E-libra
-        </h1>
-        <img src={require("./LoginPage/Logo.png")}
-            alt="logo"
-            width={100}
-            height={100} />
-        </div>
+        {isLoading ? (
+          <div className="flex flex-col lg:text-5xl text-gray-600 font-semibold tracking-wide font-mono ml-4 animate-pulse">
+            <p className="loader"></p>
+            {/* <h1 className=" mt-10">E-libra</h1> */}
+            <img
+              src={require("./LoginPage/Logo.png")}
+              alt="logo"
+              width={100}
+              height={100}
+              className="items-center ml-4"
+            />
+          </div>
+        ) : (
+          <div className="flex lg:text-5xl text-gray-600 font-semibold tracking-wide font-mono ml-4">
+            <h1 className="flex mt-10">E-libra</h1>
+            <img src={require("./LoginPage/Logo.png")} alt="logo" width={100} height={100} />
+          </div>
+        )}
       </div>
     </div>
   );
