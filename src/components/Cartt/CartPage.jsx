8c5fa9b5cf4ff54b7c473095d8d6df98f5a5d3ab
JackyDom <sopheaoudom8@gmail.com>
2023-12-23 12:41:31 +0700

    update

--- src/components/Cartt/CartPage.jsx
+++ src/components/Cartt/CartPage.jsx
@@ -105,75 +105,84 @@ const CartPage = () => {
   const total = cartItems.reduce((acc, item) => acc + parseFloat(item.price) * (item.quantity || 1), 0);
 
   return (
-    <div className="overflow-y-auto z-20 ">
+    <div className="overflow-y-auto z-20 h-[400px]">
       {loading ? (
-        <LoadingPage /> // Show LoadingPage while data is being fetched
+        <LoadingPage />
       ) : (
         <>
-          <div className="mx-auto mt-10 px-10 py-4 h-[950px]">
-            {cartItems.map((item) => (
-              <div
-                key={item.id}
-                className={`flex items-center border-b-2 py-4 mb-5 border rounded-lg border-gray-300 ${
-                  selectedItems.includes(item.id) ? "bg-blue-200 text-white" : "bg-gray-100"
-                }`}
-                onClick={(event) => handleImageClick(item, event)}
-              >
-                <img
-                  src={item.img}
-                  alt="Book"
-                  className={`w-30 h-40 ml-3 ${
-                    selectedItems.includes(item.id) ? "border-4 border-green-500" : ""
-                  }`}
-                />
-                <div className="flex-grow ml-3">
-                  <div className="flex justify-between items-center">
-                    <div>
-                      <h2 className="text-xl font-bold">{item.title}</h2>
-                      <p className="text-gray-500">{item.price}</p>
-                      <p className="text-gray-500">Quantity: {item.quantity || 1}</p>
-                    </div>
-
-                    <div className="flex flex-col space-y-2 mr-3">
-                      <div>
-                        <button
-                          className="bg-blue-500 text-white px-4 py-2 rounded-full mr-2"
-                          onClick={() => {
-                            handleIncrease(item.id);
-                          }}
-                        >
-                          +
-                        </button>
-                        <button
-                          className="bg-gray-500 text-white px-4 py-2 rounded-full"
-                          onClick={() => handleDecrease(item.id)}
-                        >
-                          -
-                        </button>
+          <div className="mx-auto mt-10 px-10 py-4">
+            {cartItems.length === 0 ? (
+              <p>Your cart is empty.</p>
+            ) : (
+              <>
+                {cartItems.map((item) => (
+                  <div
+                    key={item.id}
+                    className={`flex items-center border-b-2 py-4 mb-5 border rounded-lg border-gray-300 ${
+                      selectedItems.includes(item.id) ? "bg-blue-200 text-white" : "bg-gray-100"
+                    }`}
+                    onClick={(event) => handleImageClick(item, event)}
+                  >
+                    <img
+                      src={item.img}
+                      alt="Book"
+                      className={`w-30 h-40 ml-3 ${
+                        selectedItems.includes(item.id) ? "border-4 border-green-500" : ""
+                      }`}
+                    />
+                    <div className="flex-grow ml-3">
+                      <div className="flex justify-between items-center">
+                        <div>
+                          <h2 className="text-xl font-bold">{item.title}</h2>
+                          <p className="text-gray-500">{item.price}</p>
+                          <p className="text-gray-500">Quantity: {item.quantity || 1}</p>
+                        </div>
+  
+                        <div className="flex flex-col space-y-2 mr-3">
+                          <div>
+                            <button
+                              className="bg-blue-500 text-white px-4 py-2 rounded-full mr-2"
+                              onClick={() => {
+                                handleIncrease(item.id);
+                              }}
+                            >
+                              +
+                            </button>
+                            <button
+                              className="bg-gray-500 text-white px-4 py-2 rounded-full"
+                              onClick={() => handleDecrease(item.id)}
+                            >
+                              -
+                            </button>
+                          </div>
+                          <button
+                            className="bg-red-600 text-white rounded-xl p-1"
+                            onClick={() => handleDelete(item.id)}
+                          >
+                            Clear
+                          </button>
+                        </div>
                       </div>
-                      <button
-                        className="bg-red-600 text-white rounded-xl p-1"
-                        onClick={() => handleDelete(item.id)}
-                      >
-                        Clear
-                      </button>
                     </div>
                   </div>
-                </div>
-              </div>
-            ))}
+                ))}
+              </>
+            )}
           </div>
         </>
       )}
-
-      <div className="flex justify-end mb-5 text-xl font-semibold mt-5 space-y-5">
-        <div className="flex-col  space-x-3">
-          Total Price: {total} {"រៀល"}
-          <button className="flex bg-blue-500 px-5 py-2 text-white rounded-lg shadow-lg border-2 hover:bg-blue-600">
-            <CiCreditCard1 className="flex mt-[5px] mr-5" /> Pay now
-          </button>
+  
+      {cartItems.length > 0 && (
+        <div className="flex justify-end text-xl font-semibold mt-5 space-y-5">
+          <div className="flex-col  space-x-3">
+            Total Price: {total} {"រៀល"}
+            <button className="flex bg-blue-500 px-5 py-2 text-white rounded-lg shadow-lg border-2 hover:bg-blue-600">
+              <CiCreditCard1 className="flex mt-[5px] mr-5" /> Pay now
+            </button>
+          </div>
         </div>
-      </div>
+      )}
+  
       {showConfirmationModal && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75">
           <div className="bg-white p-4 rounded-md">
@@ -197,6 +206,7 @@ const CartPage = () => {
       )}
     </div>
   );
+  
 };
 
 export default CartPage;
