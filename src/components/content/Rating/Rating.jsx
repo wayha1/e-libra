3e032bd129adf292fe7fc5a4c883993bd1ff38c3
JackyDom <sopheaoudom8@gmail.com>
2023-12-27 16:00:59 +0700

    update

--- src/components/content/Rating/Rating.jsx
+++ src/components/content/Rating/Rating.jsx
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaStar } from "react-icons/fa";
 import "./Rating.css";
 
-export const Rating = () => {
+export const Rating = ({ onRatingChange, reset }) => {
   const [rating, setRating] = useState(null);
   const [hover, setHover] = useState(null);
+
+  useEffect(() => {
+    if (reset) {
+      setRating(null);
+      setHover(null);
+    }
+  }, [reset]);
+
+  const handleRatingChange = (newRating) => {
+    setRating(newRating);
+    onRatingChange(newRating);
+  };
+
   return (
     <div className="app flex">
       {[...Array(5)].map((star, index) => {
@@ -15,7 +28,7 @@ export const Rating = () => {
               type="radio"
               name="rating"
               value={currentRating}
-              onClick={() => setRating(currentRating)}
+              onClick={() => handleRatingChange(currentRating)}
             />
             <FaStar
               size={30}
