35ff1be638d321020a0ca7c39dd78067b3d9a3c1
JackyDom <sopheaoudom8@gmail.com>
2024-01-02 16:53:30 +0700

    update

--- src/components/Cartt/CartPage.jsx
+++ src/components/Cartt/CartPage.jsx
@@ -13,11 +13,11 @@ import LoadingPage from "../content/LoadingPage/LoadingPage";
 
 const CartPage = () => {
   const [cartItems, setCartItems] = useState([]);
-  const [loading, setLoading] = useState(true); // Introduce the loading state
   const [showConfirmationModal, setShowConfirmationModal] = useState(false);
   const [selectedItemId, setSelectedItemId] = useState(null);
   const [selectedItems, setSelectedItems] = useState([]);
   const [selectedBook, setSelectedBook] = useState(null);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   const handleIncrease = async (itemId) => {
@@ -71,8 +71,8 @@ const CartPage = () => {
   };
 
   const confirmDelete = async () => {
-    setLoading(true);
     try {
+      setLoading(true);
       const itemRef = doc(db, "addtoCart", selectedItemId);
       console.log("Deleting:", selectedItemId);
       await deleteDoc(itemRef);
@@ -113,7 +113,7 @@ const CartPage = () => {
         );
       } catch (error) {
         console.error("Error fetching cart items:", error);
-      } finally {
+      }finally {
         setLoading(false);
       }
     };
@@ -131,91 +131,85 @@ const CartPage = () => {
     navigate("/payment", { state: { cartItems, selectedBook: selectedBook } });
   };
 
-
-
   return (
     <div className="overflow-y-auto z-20 ">
-      {loading ? (
-        <LoadingPage /> // Show LoadingPage while data is being fetched
-      ) : (
-        <>
-          <div className="mx-auto mt-10 px-10 py-4 h-[400px]">
-            {cartItems.length === 0 ? (
-              <div className="flex items-center justify-center h-full">
-                <p className="text-5xl book-title text-red-600">Your cart is empty.</p>
-              </div>
-            ) : (
-              <>
-                {cartItems.map((item) => (
-                  <div
-                    key={item.id}
-                    className={`flex items-center border-b-2 py-4 mb-5 border rounded-lg border-gray-300 ${
-                      selectedItems.includes(item.id)
-                        ? "bg-blue-200 text-white"
-                        : "bg-gray-100"
-                    }`}
-                    onClick={(event) => handleImageClick(item, event)}
-                  >
-                    <img
-                      src={item.img}
-                      alt="Book"
-                      className={`w-30 h-40 ml-3 ${
-                        selectedItems.includes(item.id)
-                          ? "border-4 border-green-500"
-                          : ""
-                      }`}
-                    />
-                    <div className="flex-grow ml-3">
-                      <div className="flex justify-between items-center">
-                        <div>
-                          <h2 className="text-xl font-bold">{item.title}</h2>
-                          <p className="text-gray-500">{item.price}</p>
-                          <p className="text-gray-500">
-                            Quantity: {item.quantity || 1}
-                          </p>
-                        </div>
+      {loading && <LoadingPage />}
+      <div className="mx-auto mt-10 px-10 py-4 h-[400px]">
+        {cartItems.length === 0 ? (
+          <div className="flex items-center justify-center h-full">
+            <p className="text-5xl book-title text-red-600">Your cart is empty.</p>
+          </div>
+        ) : (
+          <>
+            {cartItems.map((item) => (
+              <div
+                key={item.id}
+                className={`flex items-center border-b-2 py-4 mb-5 border rounded-lg border-gray-300 ${
+                  selectedItems.includes(item.id)
+                    ? "bg-blue-200 text-white"
+                    : "bg-gray-100"
+                }`}
+                onClick={(event) => handleImageClick(item, event)}
+              >
+                <img
+                  src={item.img}
+                  alt="Book"
+                  className={`w-30 h-40 ml-3 ${
+                    selectedItems.includes(item.id)
+                      ? "border-4 border-green-500"
+                      : ""
+                  }`}
+                />
+                <div className="flex-grow ml-3">
+                  <div className="flex justify-between items-center">
+                    <div>
+                      <h2 className="text-xl font-bold">{item.title}</h2>
+                      <p className="text-gray-500">{item.price}</p>
+                      <p className="text-gray-500">
+                        Quantity: {item.quantity || 1}
+                      </p>
+                    </div>
 
-                        <div className="flex flex-col space-y-2 mr-3">
-                          <div>
-                            <button
-                              className="bg-blue-500 text-white px-4 py-2 rounded-full mr-2"
-                              onClick={() => {
-                                handleIncrease(item.id);
-                              }}
-                            >
-                              +
-                            </button>
-                            <button
-                              className="bg-gray-500 text-white px-4 py-2 rounded-full"
-                              onClick={() => handleDecrease(item.id)}
-                            >
-                              -
-                            </button>
-                          </div>
-                          <button
-                            className="bg-red-600 text-white rounded-xl p-1"
-                            onClick={() => handleDelete(item.id)}
-                          >
-                            Clear
-                          </button>
-                        </div>
+                    <div className="flex flex-col space-y-2 mr-3">
+                      <div>
+                        <button
+                          className="bg-blue-500 text-white px-4 py-2 rounded-full mr-2"
+                          onClick={() => {
+                            handleIncrease(item.id);
+                          }}
+                        >
+                          +
+                        </button>
+                        <button
+                          className="bg-gray-500 text-white px-4 py-2 rounded-full"
+                          onClick={() => handleDecrease(item.id)}
+                        >
+                          -
+                        </button>
                       </div>
+                      <button
+                        className="bg-red-600 text-white rounded-xl p-1"
+                        onClick={() => handleDelete(item.id)}
+                      >
+                        Clear
+                      </button>
                     </div>
                   </div>
-                ))}
-              </>
-            )}
-          </div>
-        </>
-      )}
+                </div>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
 
       {cartItems.length > 0 && (
         <div className="flex justify-end text-xl font-semibold mt-5 space-y-5">
           <div className="flex-col space-x-3 mb-8">
             Total Price: {total} {"រៀល"}
-            <button className="flex bg-blue-500 px-5 py-2 text-white rounded-lg shadow-lg 
+            <button
+              className="flex bg-blue-500 px-5 py-2 text-white rounded-lg shadow-lg 
             border-2 hover:bg-blue-600"
-            onClick={() => handleReadNowClick(selectedBook)}
+              onClick={() => handleReadNowClick(selectedBook)}
             >
               <CiCreditCard1 className="flex mt-[5px] mr-5" /> Pay now
             </button>
