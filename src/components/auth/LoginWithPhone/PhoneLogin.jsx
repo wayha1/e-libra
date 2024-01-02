9df5d7597f2cc1fb37a4401a4aab5472ca46cdad
JackyDom <sopheaoudom8@gmail.com>
2024-01-02 13:57:52 +0700

    update

--- src/components/auth/LoginWithPhone/PhoneLogin.jsx
+++ src/components/auth/LoginWithPhone/PhoneLogin.jsx
@@ -11,51 +11,50 @@ function PhoneLogin() {
 
   const sendOTP = () => {
     const auth = getAuth(app);
-    const appVerifier = new RecaptchaVerifier(auth,
-      'abc', { 'size': 'invisible' })
-    signInWithPhoneNumber(auth, phone, appVerifier).then(res => {
+    const appVerifier = new RecaptchaVerifier(auth,'abc',{'size':'invisible'})
+    signInWithPhoneNumber(auth,phone,appVerifier).then(res=>{
       console.log(res)
       window.confirmationResult = res;
       console.log('otp sent')
       setIsOtp(true);
     })
-      .catch(err => {
-        console.log(err)
-      })
+    .catch(err=>{
+      console.log(err)
+    })
   }
 
   const confirmOtp = () => {
-    window.confirmationResult.confirm(code).then(res => {
+    window.confirmationResult.confirm(code).then(res=>{
       console.log(res)
       navigate('/')
     })
-      .catch(err => {
-        console.log(err)
-      })
+    .catch(err=>{
+      console.log(err)
+    })
   }
 
   return (
     <div>
-      {!isOtp ?
-        <div>
-          <h1>login with OTP</h1>
-          <input
-            onChange={(e) => { setPhone(e.target.value) }}
-            placeholder='Phone Number'
-          />
-          <div id='abc'></div>
-          <button
-            type='button'
-            onClick={sendOTP}
-            className='bg-blue-100 rounded-lg ml-5 p-2'
-          >sent otp</button>
-        </div>
-        :
-        <div>
-          <h3>Confirm OTP</h3>
-          <input type='text' onChange={(e) => { setCode(e.target.value) }} />
-          <button type='button' onClick={confirmOtp}>Submit OTP</button>
-        </div>
+      {!isOtp?
+      <div>
+      <h1>login with OTP</h1>
+      <input
+      onChange={(e) => {setPhone(e.target.value)}} 
+      placeholder='Phone Number'
+      />
+      <div id='abc'></div>
+      <button 
+      type='button'
+      onClick={sendOTP}
+      className='bg-blue-100 rounded-lg ml-5 p-2'
+      >sent otp</button>
+      </div>
+      :
+      <div>
+        <h3>Confirm OTP</h3>
+        <input type='text' onChange={(e) => {setCode(e.target.value)}}/>
+        <button type='button' onClick={confirmOtp}>Submit OTP</button>
+      </div>
       }
     </div>
   )
