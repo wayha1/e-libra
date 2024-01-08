9f50981033c1ab80af1507754aac938d062b598c
Seng Wayha <131443243+wayha1@users.noreply.github.com>
2024-01-08 10:41:47 +0700

    update

--- src/components/HomePage/BodyHomepage.jsx
+++ src/components/HomePage/BodyHomepage.jsx
@@ -7,25 +7,25 @@ import "../../App.css";
 
 const BookCard = ({ data, handleSeeMoreClick }) => (
   <div className="hover:shadow-xl w-full overflow-hidden">
-    <div className="flex flex-col bg-white items-center shadow-lg h-[350px] w-[350px] md:w-[150px] max-sm:w-[120px]">
+    <div className="flex flex-col bg-white items-center shadow-lg h-[350px] w-[350px] lg:w-[220px] md:w-[150px] max-sm:w-[120px]">
       {data.img && (
         <img
           src={data.img}
           alt="image-book"
-          className="lg:w-[320px] h-[250px] md:w-[150px] max-sm:w-[120px] hover:scale-110 duration-300"
+          className="lg:w-[200px] h-[250px] md:w-[150px] max-sm:w-[120px] duration-300 p-0 hover:bg-gray-700"
           onClick={() => handleSeeMoreClick(data.index)}
         />
       )}
 
       {data.title && (
-        <h1 className="book-title font-bold lg:text-xl max-sm:text-sm whitespace-nowrap justify-center m-2 overflow-hidden">
+        <h1 className="book-title font-bold text-gray-500 lg:text-xl max-sm:text-sm whitespace-nowrap m-2 overflow-hidden">
           {data.title}
         </h1>
       )}
 
       {data.userRating && (
-        <h1 className="font-bold lg:text-xl max-sm:text-sm whitespace-nowrap justify-center m-2 overflow-hidden">
-          Rating: {data.userRating} Star
+        <h1 className="lg:text-xl max-sm:text-sm whitespace-nowrap justify-center m-2 overflow-hidden">
+          Rating: {data.userRating}
         </h1>
       )}
     </div>
