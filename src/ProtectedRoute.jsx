8cd8bbf633e318cae548c8133ddbc0f0acdf254f
Seng Wayha <131443243+wayha1@users.noreply.github.com>
2024-01-11 16:26:01 +0700

    update

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -42,6 +42,9 @@ const ProtectedRoute = ({ children }) => {
       navigate("/unauthorized");
     }
   }, [user, userData, navigate]);
+  console.log(user);
+
+  
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
