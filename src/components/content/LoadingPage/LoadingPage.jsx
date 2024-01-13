68b3c2223f145bad614ad608186b88cb0be6be99
wayha1 <vehaseng1122@gmail.com>
2024-01-12 21:03:33 -0800

    update

--- src/components/content/LoadingPage/LoadingPage.jsx
+++ src/components/content/LoadingPage/LoadingPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./LoadingPage.css";
+
 const LoadingPage = () => {
   const [isLoading, setIsLoading] = useState(false);
 
@@ -15,17 +16,13 @@ const LoadingPage = () => {
       <div className="flex w-full h-full justify-center items-center">
         {isLoading ? (
           <div className="flex flex-col lg:text-5xl text-gray-800 font-semibold tracking-wide font-mono ml-4 animate-pulse">
-            <img
-              src={require("./Logo.png")}
-              alt="logo"
-              width={80}
-              height={80}
-              className="items-center"
-            />
+
             <div className="loader"></div>
+
           </div>
         ) : (
           <div className="flex lg:text-5xl text-gray-600 font-semibold tracking-wide font-mono ml-4">
+
             <div className="loader"></div>
           </div>
         )}
